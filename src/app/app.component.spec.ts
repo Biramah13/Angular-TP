5ebe904fbe74d6bbc8f54864f7b9b9782c8bbcc9
Biramah13 <97129655+Biramah13@users.noreply.github.com>
2022-05-24 12:52:24 +0200

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Mon-CV'`, () => {
+  it(`should have as title 'Angular-TP'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Mon-CV');
+    expect(app.title).toEqual('Angular-TP');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Mon-CV app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular-TP app is running!');
   });
 });
